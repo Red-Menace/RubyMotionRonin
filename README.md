a9e839fe372e35b8689395ab55a272b15d3e6e2f
Red-Menace <Red-Menace@users.noreply.github.com>
2024-07-11 11:02:41 -0600

    update osx template project

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 A few rogue macOS projects:
 
 - **User Defaults** - a simple example using **NSUserDefaults** and a preferences window
-- **osx template** - a cleaned up and (slightly) enhanced macOS "Hello, World" project template
+- **osx template** - a cleaned up and slightly enhanced localized (English) macOS "Hello, World" project template
 - **ExampleIB** - an example demonstrating the use of an Objective-C Xcode project for the user interface
 - **Help Book Sample** - not really RubyMotion specific, but a sample help book template that can be dropped into your projects.
 
--- osx template/files/app/menu.rb
+++ osx template/files/app/menu.rb
@@ -1,157 +1,181 @@
 
-############################################################
-#  ――― Application Delegate ―――  
-############################################################
+##################################################
+#  ―――― Class Extensions ――――
+##################################################
+
+class String
+
+   # Return a localized string.
+   # Optionally from the specified table and language bundle (en, ja, fr, etc).
+   def localized(value = nil, table = nil, languageCode = nil)
+      bundle = NSBundle.mainBundle
+      unless languageCode.nil? || languageCode == bundle.preferredLocalizations.first
+         path = NSBundle.mainBundle.pathForResource(languageCode, ofType: 'lproj')
+         bundle = NSBundle.bundleWithPath(path) unless path.nil?
+      end
+      bundle.localizedStringForKey(self, value: value, table: table).UTF8String
+   end
+
+end
+
 
 class AppDelegate
 
-   ############################################################
-   #  ――― Menu Creation Methods ―――  
-   ############################################################
+   ##################################################
+   #  ―――― Menu Creation Methods ――――
+   ##################################################
 
+   # build localized main application menu
    def buildMenu
+      appName = NSBundle.mainBundle.infoDictionary['CFBundleName']
       @mainMenu = NSMenu.new
 
-      appName = NSBundle.mainBundle.infoDictionary['CFBundleName']
-
-      find_menu = createMenu('Find') do
-         addItemWithTitle('Find...', action: 'performFindPanelAction:', keyEquivalent: 'f').tag = 1
-         addItemWithTitle('Find Next', action: 'performFindPanelAction:', keyEquivalent: 'g').tag = 2
-         addItemWithTitle('Find Previous', action: 'performFindPanelAction:', keyEquivalent: 'G').tag = 3
-         addItemWithTitle('Use Selection for Find', action: 'performFindPanelAction:', keyEquivalent: 'e').tag = 7
-         addItemWithTitle('Jump to Selection', action: 'centerSelectionInVisibleArea:', keyEquivalent: 'j')
-      end
-
-      spelling_and_grammar_menu = createMenu('Spelling and Grammar') do
-         addItemWithTitle('Show Spelling and Grammar', action: 'showGuessPanel:', keyEquivalent: ':')
-         addItemWithTitle('Check Document Now', action: 'checkSpelling:', keyEquivalent: ';')
+      appMenu = addMenu(NSBundle.mainBundle.objectForInfoDictionaryKey('CFBundleName')) do
+         addItemWithTitle("#{'MAIN:ABOUT'.localized} #{appName}", action: 'orderFrontStandardAboutPanel:', keyEquivalent: '')
          addItem(NSMenuItem.separatorItem)
-         addItemWithTitle('Check Spelling While Typing', action: 'toggleContinuousSpellChecking:', keyEquivalent: '')
-         addItemWithTitle('Check Grammar With Spelling', action: 'toggleGrammarChecking:', keyEquivalent: '')
-         addItemWithTitle('Correct Spelling Automatically', action: 'toggleAutomaticSpellingCorrection:', keyEquivalent: '')
-      end
-
-      substitutions_menu = createMenu('Substitutions') do
-         addItemWithTitle('Show Substitutions', action: 'orderFrontSubstitutionsPanel:', keyEquivalent: 'f')
+         addItemWithTitle('MAIN:PREFS'.localized, action: 'openPreferences:', keyEquivalent: ',')
          addItem(NSMenuItem.separatorItem)
-         addItemWithTitle('Smart Copy/Paste', action: 'toggleSmartInsertDelete:', keyEquivalent: 'f')
-         addItemWithTitle('Smart Quotes', action: 'toggleAutomaticQuoteSubstitution:', keyEquivalent: 'g')
-         addItemWithTitle('Smart Dashes', action: 'toggleAutomaticDashSubstitution:', keyEquivalent: '')
-         addItemWithTitle('Smart Links', action: 'toggleAutomaticLinkDetection:', keyEquivalent: 'G')
-         addItemWithTitle('Text Replacement', action: 'toggleAutomaticTextReplacement:', keyEquivalent: '')
-      end
-
-      transformations_menu = createMenu('Transformations') do
-         addItemWithTitle('Make Upper Case', action: 'uppercaseWord:', keyEquivalent: '')
-         addItemWithTitle('Make Lower Case', action: 'lowercaseWord:', keyEquivalent: '')
-         addItemWithTitle('Capitalize', action: 'capitalizeWord:', keyEquivalent: '')
-      end
-
-      speech_menu = createMenu('Speech') do
-         addItemWithTitle('Start Speaking', action: 'startSpeaking:', keyEquivalent: '')
-         addItemWithTitle('Stop Speaking', action: 'stopSpeaking:', keyEquivalent: '')
-      end
-
-      addMenu(appName) do
-         addItemWithTitle("About #{appName}", action: 'orderFrontStandardAboutPanel:', keyEquivalent: '')
-         addItem(NSMenuItem.separatorItem)
-         addItemWithTitle('Preferences', action: 'openPreferences:', keyEquivalent: ',')
-         addItem(NSMenuItem.separatorItem)
-         servicesItem = addItemWithTitle('Services', action: nil, keyEquivalent: '')
+         servicesItem = addItemWithTitle('MAIN:SERVICES'.localized, action: nil, keyEquivalent: '')
          NSApp.servicesMenu = servicesItem.submenu = NSMenu.new
          addItem(NSMenuItem.separatorItem)
-         addItemWithTitle("Hide #{appName}", action: 'hide:', keyEquivalent: 'h')
-         item = addItemWithTitle('Hide Others', action: 'hideOtherApplications:', keyEquivalent: 'H')
+         addItemWithTitle("#{'MAIN:HIDE'.localized} #{appName}", action: 'hide:', keyEquivalent: 'h')
+         item = addItemWithTitle('MAIN:OTHERS'.localized, action: 'hideOtherApplications:', keyEquivalent: 'H')
          item.keyEquivalentModifierMask = NSCommandKeyMask | NSAlternateKeyMask
-         addItemWithTitle('Show All', action: 'unhideAllApplications:', keyEquivalent: '')
+         addItemWithTitle('MAIN:SHOW'.localized, action: 'unhideAllApplications:', keyEquivalent: '')
          addItem(NSMenuItem.separatorItem)
-         addItemWithTitle("Quit #{appName}", action: 'terminate:', keyEquivalent: 'q')
+         addItemWithTitle("#{'MAIN:QUIT'.localized} #{appName}", action: 'terminate:', keyEquivalent: 'q')
+      end
+            
+      fileMenu = addMenu('FILE:MENU'.localized) do
+         addItemWithTitle('FILE:NEW'.localized, action: 'newDocument:', keyEquivalent: 'n')
+         addItemWithTitle('FILE:OPEN'.localized, action: 'openDocument:', keyEquivalent: 'o')
+         addItem(NSMenuItem.separatorItem)
+         addItemWithTitle('FILE:CLOSE'.localized, action: 'performClose:', keyEquivalent: 'w')
+         addItemWithTitle('FILE:SAVE'.localized, action: 'saveDocument:', keyEquivalent: 's')
+         addItemWithTitle('FILE:REVERT'.localized, action: 'revertDocumentToSaved:', keyEquivalent: '')
+         addItem(NSMenuItem.separatorItem)
+         addItemWithTitle('FILE:SETUP'.localized, action: 'runPageLayout:', keyEquivalent: 'P')
+         addItemWithTitle('FILE:PRINT'.localized, action: 'printDocument:', keyEquivalent: 'p')
+      end
+      
+      findMenu = createMenu('FIND:MENU'.localized) do  # Edit submenu
+         addItemWithTitle('FIND:FIND'.localized, action: 'performFindPanelAction:', keyEquivalent: 'f').tag = 1
+         addItemWithTitle('FIND:NEXT'.localized, action: 'performFindPanelAction:', keyEquivalent: 'g').tag = 2
+         addItemWithTitle('FIND:PREVIOUS'.localized, action: 'performFindPanelAction:', keyEquivalent: 'G').tag = 3
+         addItemWithTitle('FIND:SELECTION'.localized, action: 'performFindPanelAction:', keyEquivalent: 'e').tag = 7
+         addItemWithTitle('FIND:JUMP'.localized, action: 'centerSelectionInVisibleArea:', keyEquivalent: 'j')
       end
 
-      addMenu('File') do
-         addItemWithTitle('New', action: 'newDocument:', keyEquivalent: 'n')
-         addItemWithTitle('Open…', action: 'openDocument:', keyEquivalent: 'o')
+      spelling_and_grammar_menu = createMenu('SPELL:MENU'.localized) do  # Edit submenu
+         addItemWithTitle('SPELL:SHOW'.localized, action: 'showGuessPanel:', keyEquivalent: ':')
+         addItemWithTitle('SPELL:DOCUMENT'.localized, action: 'checkSpelling:', keyEquivalent: ';')
          addItem(NSMenuItem.separatorItem)
-         addItemWithTitle('Close', action: 'performClose:', keyEquivalent: 'w')
-         addItemWithTitle('Save…', action: 'saveDocument:', keyEquivalent: 's')
-         addItemWithTitle('Revert to Saved', action: 'revertDocumentToSaved:', keyEquivalent: '')
-         addItem(NSMenuItem.separatorItem)
-         addItemWithTitle('Page Setup…', action: 'runPageLayout:', keyEquivalent: 'P')
-         addItemWithTitle('Print…', action: 'printDocument:', keyEquivalent: 'p')
+         addItemWithTitle('SPELL:SPELLING'.localized, action: 'toggleContinuousSpellChecking:', keyEquivalent: '')
+         addItemWithTitle('SPELL:GRAMMAR'.localized, action: 'toggleGrammarChecking:', keyEquivalent: '')
+         addItemWithTitle('SPELL:CORRECT'.localized, action: 'toggleAutomaticSpellingCorrection:', keyEquivalent: '')
       end
 
-      addMenu('Edit') do
-         addItemWithTitle('Undo', action: 'undo:', keyEquivalent: 'z')
-         addItemWithTitle('Redo', action: 'redo:', keyEquivalent: 'Z')
+      substitutions_menu = createMenu('SUBS:MENU'.localized) do  # Edit submenu
+         addItemWithTitle('SUBS:SHOW'.localized, action: 'orderFrontSubstitutionsPanel:', keyEquivalent: 'f')
          addItem(NSMenuItem.separatorItem)
-         addItemWithTitle('Cut', action: 'cut:', keyEquivalent: 'x')
-         addItemWithTitle('Copy', action: 'copy:', keyEquivalent: 'c')
-         addItemWithTitle('Paste', action: 'paste:', keyEquivalent: 'v')
-         item = addItemWithTitle('Paste and Match Style', action: 'pasteAsPlainText:', keyEquivalent: 'V')
+         addItemWithTitle('SUBS:COPY'.localized, action: 'toggleSmartInsertDelete:', keyEquivalent: 'f')
+         addItemWithTitle('SUBS:QUOTES'.localized, action: 'toggleAutomaticQuoteSubstitution:', keyEquivalent: 'g')
+         addItemWithTitle('SUBS:DASHES'.localized, action: 'toggleAutomaticDashSubstitution:', keyEquivalent: '')
+         addItemWithTitle('SUBS:LINKS'.localized, action: 'toggleAutomaticLinkDetection:', keyEquivalent: 'G')
+         addItemWithTitle('SUBS:REPLACE'.localized, action: 'toggleAutomaticTextReplacement:', keyEquivalent: '')
+      end
+
+      transformationsMenu = createMenu('TRANS:MENU'.localized) do  # Edit submenu
+         addItemWithTitle('TRANS:UPPER'.localized, action: 'uppercaseWord:', keyEquivalent: '')
+         addItemWithTitle('TRANS:LOWER'.localized, action: 'lowercaseWord:', keyEquivalent: '')
+         addItemWithTitle('TRANS:CAPS'.localized, action: 'capitalizeWord:', keyEquivalent: '')
+      end
+
+      speechMenu = createMenu('SPEECH:MENU'.localized) do  # Edit submenu
+         addItemWithTitle('SPEECH:START'.localized, action: 'startSpeaking:', keyEquivalent: '')
+         addItemWithTitle('SPEECH:STOP'.localized, action: 'stopSpeaking:', keyEquivalent: '')
+      end
+
+      editMenu = addMenu('EDIT:MENU'.localized) do
+         addItemWithTitle('EDIT:UNDO'.localized, action: 'undo:', keyEquivalent: 'z')
+         addItemWithTitle('EDIT:REDO'.localized, action: 'redo:', keyEquivalent: 'Z')
+         addItem(NSMenuItem.separatorItem)
+         addItemWithTitle('EDIT:CUT'.localized, action: 'cut:', keyEquivalent: 'x')
+         addItemWithTitle('EDIT:COPY'.localized, action: 'copy:', keyEquivalent: 'c')
+         addItemWithTitle('EDIT:PASTE'.localized, action: 'paste:', keyEquivalent: 'v')
+         item = addItemWithTitle('EDIT:MATCH'.localized, action: 'pasteAsPlainText:', keyEquivalent: 'V')
          item.keyEquivalentModifierMask = NSCommandKeyMask | NSAlternateKeyMask
-         addItemWithTitle('Delete', action: 'delete:', keyEquivalent: '')
-         addItemWithTitle('Select All', action: 'selectAll:', keyEquivalent: 'a')
+         addItemWithTitle('EDIT:DELETE'.localized, action: 'delete:', keyEquivalent: '')
+         addItemWithTitle('EDIT:SELECT'.localized, action: 'selectAll:', keyEquivalent: 'a')
          addItem(NSMenuItem.separatorItem)
-         addItem(find_menu)
+         addItem(findMenu)
          addItem(spelling_and_grammar_menu)
          addItem(substitutions_menu)
-         addItem(transformations_menu)
-         addItem(speech_menu)
+         addItem(transformationsMenu)
+         addItem(speechMenu)
       end
 
-      textMenu = createMenu('Text') do
-         addItemWithTitle('Align Left', action: 'alignLeft:', keyEquivalent: '{')
-         addItemWithTitle('Center', action: 'alignCenter:', keyEquivalent: '|')
-         addItemWithTitle('Justify', action: 'alignJustified:', keyEquivalent: '')
-         addItemWithTitle('Align Right', action: 'alignRight:', keyEquivalent: '}')
+      textMenu = createMenu('TEXT:MENU'.localized) do  # Format submenu
+         addItemWithTitle('TEXT:LEFT'.localized, action: 'alignLeft:', keyEquivalent: '{')
+         addItemWithTitle('TEXT:CENTER'.localized, action: 'alignCenter:', keyEquivalent: '|')
+         addItemWithTitle('TEXT:JUSTIFY'.localized, action: 'alignJustified:', keyEquivalent: '')
+         addItemWithTitle('TEXT:RIGHT'.localized, action: 'alignRight:', keyEquivalent: '}')
          addItem(NSMenuItem.separatorItem)
-         addItemWithTitle('Show Ruler', action: 'toggleRuler:', keyEquivalent: '')
-         item = addItemWithTitle('Copy Ruler', action: 'copyRuler:', keyEquivalent: 'c')
+         addItemWithTitle('TEXT:SHOW'.localized, action: 'toggleRuler:', keyEquivalent: '')
+         item = addItemWithTitle('TEXT:COPY'.localized, action: 'copyRuler:', keyEquivalent: 'c')
          item.keyEquivalentModifierMask = NSCommandKeyMask | NSControlKeyMask
-         item = addItemWithTitle('Paste Ruler', action: 'pasteRuler:', keyEquivalent: 'v')
+         item = addItemWithTitle('TEXT:PASTE'.localized, action: 'pasteRuler:', keyEquivalent: 'v')
          item.keyEquivalentModifierMask = NSCommandKeyMask | NSControlKeyMask
       end
 
-      addMenu('Format') do
-         item = addItemWithTitle('Font', action: nil, keyEquivalent: '')
-         item.submenu = NSFontManager.sharedFontManager.fontMenu(true)
-         addItem textMenu
+      formatMenu = addMenu('FORMAT:MENU'.localized) do
+         item = addItemWithTitle('FONT:MENU'.localized, action: nil, keyEquivalent: '')
+         item.submenu = NSFontManager.sharedFontManager.fontMenu(true)  # use system font menu
+         addItem(textMenu)
       end
-
-      addMenu('View') do
-         item = addItemWithTitle('Show Toolbar', action: 'toggleToolbarShown:', keyEquivalent: 't')
+      
+      addMenu('VIEW:MENU'.localized) do
+         item = addItemWithTitle('VIEW:SHOW'.localized, action: 'toggleToolbarShown:', keyEquivalent: 't')
          item.keyEquivalentModifierMask = NSCommandKeyMask | NSAlternateKeyMask
-         addItemWithTitle('Customize Toolbar…', action: 'runToolbarCustomizationPalette:', keyEquivalent: '')
+         addItemWithTitle('VIEW:CUSTOMIZE'.localized, action: 'runToolbarCustomizationPalette:', keyEquivalent: '')
       end
 
-      NSApp.windowsMenu = addMenu('Window') do
-         addItemWithTitle('Minimize', action: 'performMiniaturize:', keyEquivalent: 'm')
-         addItemWithTitle('Zoom', action: 'performZoom:', keyEquivalent: '')
-         addItem(NSMenuItem.separatorItem)
-         addItemWithTitle('Bring All To Front', action: 'arrangeInFront:', keyEquivalent: '')
+      NSApp.windowsMenu = addMenu('WINDOW:MENU'.localized) do
+         addItemWithTitle('WINDOW:MINIMIZE'.localized, action: 'performMiniaturize:', keyEquivalent: 'm')
+         addItemWithTitle('WINDOW:ZOOM'.localized, action: 'performZoom:', keyEquivalent: '')
+         addItem(NSMenuItem.separatorItem)  # just added items
+         addItemWithTitle('WINDOW:FRONT'.localized, action: 'arrangeInFront:', keyEquivalent: '')
       end.submenu
-
-      NSApp.helpMenu = addMenu('Help') do
-         addItemWithTitle("#{appName} Help", action: 'showHelp:', keyEquivalent: '?')
+      
+      NSApp.helpMenu = addMenu('HELP:MENU'.localized) do
+         addItemWithTitle("#{appName} #{'HELP:HELP'.localized}", action: 'showHelp:', keyEquivalent: '?')
       end.submenu
 
       NSApp.mainMenu = @mainMenu
+      
    end
 
+
+   ##################################################
+   #  ―――― Private/Protected Methods ――――
+   ##################################################
 
    private
 
    def addMenu(title, &block)
-      createMenu(title, &block).tap { |obj| @mainMenu.addItem obj }
+      createMenu(title, &block).tap do |item|
+         @mainMenu.addItem item
+      end
    end
-
-
+   
+   
    def createMenu(title, &block)
       menu = NSMenu.alloc.initWithTitle(title)
       menu.instance_eval(&block) if block
-      NSMenuItem.alloc
-                .initWithTitle(title, action: nil, keyEquivalent: '')
-                .tap { |obj| obj.submenu = menu }
+      NSMenuItem.alloc.initWithTitle(title, action: nil, keyEquivalent: '').tap do |item|
+         item.submenu = menu
+      end
    end
 
 end
+
